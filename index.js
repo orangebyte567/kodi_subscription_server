bd68400cb4e0a41ae4f7de687e36ae94a268ab0b
dennis <dennis.eckert0218@gmail.com>
2024-07-26 14:04:54 +0200

    change index.js

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ dotenv.config();
 const app = express();
 app.use(cors());
 // app.use(express.json());
+// app.use(bodyParser.json()); 
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true })); 
 
