c401c1051aa2df57ccdf8be8bd62e235aa5acad9
dennis <dennis.eckert0218@gmail.com>
2024-07-26 16:08:29 +0200

    change vercel.json

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ dotenv.config();
 const app = express();
 app.use(cors());
 // app.use(express.json());
-// app.use(bodyParser.json()); 
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true })); 
 
