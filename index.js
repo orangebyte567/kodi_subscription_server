0fa958c5128b8090c84887912255a2fdbce68392
dennis <dennis.eckert0218@gmail.com>
2024-07-26 12:44:44 +0200

    change index.js

--- index.js
+++ index.js
@@ -13,10 +13,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => console.log('MongoDB connected'))
+mongoose.connect(process.env.MONGO_URI, {}).then(() => console.log('MongoDB connected'))
   .catch(err => console.error(err));
 
 app.get('/', (req, res) => {
